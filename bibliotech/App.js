c1acce4325851abed990d99d5319689397aa987e
Murilo Alves Zapiello <54750759805@info.local>
2024-05-28 14:51:09 -0300

    🏃

--- bibliotech/App.js
+++ bibliotech/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Navigation } from "./src/screens/Navigation/Navigation";
+import {
+  useFonts,
+  MontserratAlternates_100Thin,
+  MontserratAlternates_200ExtraLight,
+  MontserratAlternates_100Thin_Italic,
+  MontserratAlternates_200ExtraLight_Italic,
+  MontserratAlternates_300Light,
+  MontserratAlternates_300Light_Italic,
+  MontserratAlternates_400Regular,
+  MontserratAlternates_400Regular_Italic,
+  MontserratAlternates_500Medium,
+  MontserratAlternates_500Medium_Italic,
+  MontserratAlternates_600SemiBold,
+  MontserratAlternates_600SemiBold_Italic,
+  MontserratAlternates_700Bold,
+  MontserratAlternates_700Bold_Italic,
+  MontserratAlternates_800ExtraBold,
+  MontserratAlternates_800ExtraBold_Italic,
+  MontserratAlternates_900Black,
+  MontserratAlternates_900Black_Italic,
+} from "@expo-google-fonts/montserrat-alternates";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    MontserratAlternates_400Regular,
+    MontserratAlternates_600SemiBold,
+    MontserratAlternates_500Medium,
+    MontserratAlternates_700Bold,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Navegation"
+          component={Navigation}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ bibliotech/src/screens/Navigation/Navigation.js
@@ -0,0 +1,9 @@
+import { Button, View } from "react-native";
+
+export const Navigation = () => {
+    return(
+        <View>
+            <Button />
+        </View>
+    );
+}
